82dfdf9db4e73d8750e8a69d11e11fa4f11a9eb0
jbphet <john.blanco@colorado.edu>
2021-01-13 13:05:59 -0700

    prevent simultaneous drag of point controllers, see https://github.com/phetsims/number-line-operations/issues/80

--- js/common/view/ControllableOperationNumberLineNode.js
+++ js/common/view/ControllableOperationNumberLineNode.js
@@ -43,16 +43,62 @@ class ControllableOperationNumberLineNode extends Node {
     this.addChild( numberLineNode );
 
     // point controller for the starting point on the number line, which is always present
-    pointControllerLayer.addChild( new PointControllerNode( initialValuePointController ) );
+    const initialValuePointControllerNode = new PointControllerNode( initialValuePointController );
+    pointControllerLayer.addChild( initialValuePointControllerNode );
+
+    // map of point controllers to nodes
+    const pointControllerToNodeMap = new Map( [ [ initialValuePointController, initialValuePointControllerNode ] ] );
+
+    // point controller that is currently dragging if any, only one is allowed at a time
+    let draggingPointController = null;
+
+    // Define a closure that will update the pickable setting for the point controllers.  This exists because it's far
+    // simpler to prevent simultaneous movements of the point controllers (which can occur in multi-touch environments)
+    // than it is to support it.
+    const updatePointControllerPickability = () => {
+
+      // Check and update the dragging point controller variable.
+      if ( !draggingPointController || !draggingPointController.isDraggingProperty.value ) {
+        const allPointControllers = [ initialValuePointController, ...pointControllerObservableArray ];
+        draggingPointController = allPointControllers.find( pc => pc.isDraggingProperty.value );
+      }
+
+      // Update the state of the point controller nodes based on which, if any, is currently being dragged.
+      if ( draggingPointController ) {
+
+        pointControllerToNodeMap.forEach( ( node, pointController ) => {
+
+          // Make sure that only the point controller being dragged is pickable.
+          node.pickable = pointController === draggingPointController;
+
+          // It's unlikely, but possible, for two controllers to get picked up in the same animation frame.  If that
+          // happened and there is more than one point controller being dragged, cancel the interactions with any that
+          // are not designated as the dragging point controller.
+          if ( pointController !== draggingPointController && pointController.isDraggingProperty.value ) {
+            node.interruptSubtreeInput();
+          }
+        } );
+      }
+      else {
+
+        // None of the point controllers are being dragged, so make sure all the point controller nodes are pickable.
+        pointControllerToNodeMap.forEach( node => { node.pickable = true; } );
+      }
+    };
+    initialValuePointController.isDraggingProperty.link( updatePointControllerPickability );
 
     // Add and remove nodes for the point controllers that come and go from the number line.
     pointControllerObservableArray.addItemAddedListener( addedPointController => {
       const pointControllerNode = new PointControllerNode( addedPointController );
+      pointControllerToNodeMap.set( addedPointController, pointControllerNode );
       pointControllerLayer.addChild( pointControllerNode );
+      addedPointController.isDraggingProperty.link( updatePointControllerPickability );
       const removalListener = removedPointController => {
         if ( removedPointController === addedPointController ) {
           pointControllerLayer.removeChild( pointControllerNode );
           pointControllerNode.dispose();
+          pointControllerToNodeMap.delete( removedPointController );
+          removedPointController.isDraggingProperty.unlink( updatePointControllerPickability );
           pointControllerObservableArray.removeItemRemovedListener( removalListener );
         }
       };
