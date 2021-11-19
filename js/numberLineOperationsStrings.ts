25ae9aaddba0e6340a4611759a6a6cf21e188f95
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:31 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/numberLineOperationsStrings.ts
@@ -0,0 +1,57 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import numberLineOperations from './numberLineOperations.js';
+
+type StringsType = {
+  'number-line-operations': {
+    'title': string
+  },
+  'screen': {
+    'netWorth': string,
+    'operations': string,
+    'generic': string,
+    'chips': string
+  },
+  'operationLabels': string,
+  'operationDescriptions': string,
+  'tickMarks': string,
+  'netWorth': string,
+  'netWorthWithCurrencyPattern': string,
+  'total': string,
+  'totalValuePattern': string,
+  'totalCurrencyPattern': string,
+  'currencyUnits': string,
+  'assets': string,
+  'asset': string,
+  'debts': string,
+  'debt': string,
+  'positives': string,
+  'negatives': string,
+  'currencyValuePattern': string,
+  'add': string,
+  'remove': string,
+  'addRemoveZeroPattern': string,
+  'addRemovePositiveNegativePattern': string,
+  'positive': string,
+  'negative': string,
+  'addRemoveZeroCurrencyPattern': string,
+  'addRemoveAssetDebtPattern': string,
+  'pointLabels': string,
+  'simplify': string,
+  'initialNetWorth': string,
+  'initialNetWorthWithBreak': string,
+  'operationOffScale': string,
+  'numericalExpression': string,
+  'loan': string
+};
+
+const numberLineOperationsStrings = getStringModule( 'NUMBER_LINE_OPERATIONS' ) as StringsType;
+
+numberLineOperations.register( 'numberLineOperationsStrings', numberLineOperationsStrings );
+
+export default numberLineOperationsStrings;
