eff174bc2c26fb5f74bb03c5744a3194ae11ba0d
phet-dev <phetadmin@gmail.com>
2022-01-18 03:28:21 -0700

    update copyright dates from daily grunt work

--- js/net-worth/view/BalanceSheetItemNode.js
+++ js/net-worth/view/BalanceSheetItemNode.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * BalanceSheetItemNode is the view representation of a ValueItem, which is the general term being used in this
