4c5e587fb0f9d9c36595d1fb23ace30508163139
phet-dev <phetadmin@gmail.com>
2022-06-24 03:59:38 -0600

    update copyright dates from daily grunt work

--- js/net-worth/view/NLONetWorthScreenView.js
+++ js/net-worth/view/NLONetWorthScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * NLONetWorthScreenView is the root of the view screen graph for the Net Worth screen.
