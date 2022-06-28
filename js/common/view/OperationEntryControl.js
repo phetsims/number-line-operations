1e7a37676bd12cc1b12be76020fd607774181443
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-28 16:08:22 -0600

    Move NumberPicker to sun, see: https://github.com/phetsims/sun/issues/436

--- js/common/view/OperationEntryControl.js
+++ js/common/view/OperationEntryControl.js
@@ -15,7 +15,7 @@ import Range from '../../../../dot/js/Range.js';
 import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
-import NumberPicker from '../../../../scenery-phet/js/NumberPicker.js';
+import NumberPicker from '../../../../sun/js/NumberPicker.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import { HBox } from '../../../../scenery/js/imports.js';
 import { Path } from '../../../../scenery/js/imports.js';
