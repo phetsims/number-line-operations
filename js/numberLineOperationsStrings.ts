07e5f8d515786c9d3a7e7c4145abf3819bae61a9
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/numberLineOperationsStrings.ts
+++ js/numberLineOperationsStrings.ts
@@ -5,49 +5,86 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import numberLineOperations from './numberLineOperations.js';
 
 type StringsType = {
   'number-line-operations': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'netWorth': string;
+    'netWorthProperty': TReadOnlyProperty<string>;
     'operations': string;
+    'operationsProperty': TReadOnlyProperty<string>;
     'generic': string;
+    'genericProperty': TReadOnlyProperty<string>;
     'chips': string;
+    'chipsProperty': TReadOnlyProperty<string>;
   };
   'operationLabels': string;
+  'operationLabelsProperty': TReadOnlyProperty<string>;
   'operationDescriptions': string;
+  'operationDescriptionsProperty': TReadOnlyProperty<string>;
   'tickMarks': string;
+  'tickMarksProperty': TReadOnlyProperty<string>;
   'netWorth': string;
+  'netWorthProperty': TReadOnlyProperty<string>;
   'netWorthWithCurrencyPattern': string;
+  'netWorthWithCurrencyPatternProperty': TReadOnlyProperty<string>;
   'total': string;
+  'totalProperty': TReadOnlyProperty<string>;
   'totalValuePattern': string;
+  'totalValuePatternProperty': TReadOnlyProperty<string>;
   'totalCurrencyPattern': string;
+  'totalCurrencyPatternProperty': TReadOnlyProperty<string>;
   'currencyUnits': string;
+  'currencyUnitsProperty': TReadOnlyProperty<string>;
   'assets': string;
+  'assetsProperty': TReadOnlyProperty<string>;
   'asset': string;
+  'assetProperty': TReadOnlyProperty<string>;
   'debts': string;
+  'debtsProperty': TReadOnlyProperty<string>;
   'debt': string;
+  'debtProperty': TReadOnlyProperty<string>;
   'positives': string;
+  'positivesProperty': TReadOnlyProperty<string>;
   'negatives': string;
+  'negativesProperty': TReadOnlyProperty<string>;
   'currencyValuePattern': string;
+  'currencyValuePatternProperty': TReadOnlyProperty<string>;
   'add': string;
+  'addProperty': TReadOnlyProperty<string>;
   'remove': string;
+  'removeProperty': TReadOnlyProperty<string>;
   'addRemoveZeroPattern': string;
+  'addRemoveZeroPatternProperty': TReadOnlyProperty<string>;
   'addRemovePositiveNegativePattern': string;
+  'addRemovePositiveNegativePatternProperty': TReadOnlyProperty<string>;
   'positive': string;
+  'positiveProperty': TReadOnlyProperty<string>;
   'negative': string;
+  'negativeProperty': TReadOnlyProperty<string>;
   'addRemoveZeroCurrencyPattern': string;
+  'addRemoveZeroCurrencyPatternProperty': TReadOnlyProperty<string>;
   'addRemoveAssetDebtPattern': string;
+  'addRemoveAssetDebtPatternProperty': TReadOnlyProperty<string>;
   'pointLabels': string;
+  'pointLabelsProperty': TReadOnlyProperty<string>;
   'simplify': string;
+  'simplifyProperty': TReadOnlyProperty<string>;
   'initialNetWorth': string;
+  'initialNetWorthProperty': TReadOnlyProperty<string>;
   'initialNetWorthWithBreak': string;
+  'initialNetWorthWithBreakProperty': TReadOnlyProperty<string>;
   'operationOffScale': string;
+  'operationOffScaleProperty': TReadOnlyProperty<string>;
   'numericalExpression': string;
+  'numericalExpressionProperty': TReadOnlyProperty<string>;
   'loan': string;
+  'loanProperty': TReadOnlyProperty<string>;
 };
 
 const numberLineOperationsStrings = getStringModule( 'NUMBER_LINE_OPERATIONS' ) as StringsType;
